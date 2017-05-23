eb55275537332f600c8dfc7af9b73b8b35bdacac
William Carder <pcarder7@gmail.com>
2017-05-22 20:50:26 -0400

    Just another push

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,9 @@ public class HelloWorld {
     public static void main(String[] args){
             System.out.println("Hello World!");
             System.out.println("I'm just learning how to use git.");
+            for(int i = 0; i <10; i ++){
+                System.out.println("Testing git " + i);
+            }
 
     }
 }
